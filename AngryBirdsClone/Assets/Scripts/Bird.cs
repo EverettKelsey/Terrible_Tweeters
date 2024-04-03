d24ea080a079a289b53519e32600eb0a505f2491
EverettKelsey <kelseyj@my.erau.edu>
2024-04-02 17:14:08 -0700

    progress????

---
+++ AngryBirdsClone/Assets/Scripts/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
